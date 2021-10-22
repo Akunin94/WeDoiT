bbcb25a94ccea700afeacc50215dfdf5648a4d9f
Usmonov Davron <akunin94@gmail.com>
2021-10-22 21:27:29 +0500

    End day5

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,43 @@
-// // Import vendor jQuery plugin example
-// import '~/app/libs/mmenu/dist/mmenu.js'
+import $ from "jquery";
+import "slick-carousel";
 
-document.addEventListener('DOMContentLoaded', () => {
+// JQUERY BIG FIX +++++
+jQuery.event.special.touchstart = {
+	setup: function (_, ns, handle) {
+		this.addEventListener("touchstart", handle, { passive: !ns.includes("noPreventDefault") });
+	},
+};
+jQuery.event.special.touchmove = {
+	setup: function (_, ns, handle) {
+		this.addEventListener("touchmove", handle, { passive: !ns.includes("noPreventDefault") });
+	},
+};
+jQuery.event.special.wheel = {
+	setup: function (_, ns, handle) {
+		this.addEventListener("wheel", handle, { passive: true });
+	},
+};
+jQuery.event.special.mousewheel = {
+	setup: function (_, ns, handle) {
+		this.addEventListener("mousewheel", handle, { passive: true });
+	},
+};
+// JQUERY BIG FIX -----
 
-	// Custom JS
-
-})
+document.addEventListener("DOMContentLoaded", () => {
+	// REVIEWS SLIDER +++++
+	$(".wedoit-double__reviews-slider").each(function () {
+		if ($(this).find(".wedoit-double__reviews-slider-item").length > 1) {
+			$(this).slick({
+				dots: false,
+				autoplay: true,
+				autoplaySpeed: 10000,
+				arrows: true,
+				infinite: true,
+				pauseOnHover: false,
+				rows: 0,
+			});
+		}
+	});
+	// REVIEWS SLIDER -----
+});
